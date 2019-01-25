a4973b14a7b27d1e1c4db37c4da4b6c802adf03b
keaton-huberty <30304328+keaton-huberty@users.noreply.github.com>
2019-01-25 16:20:38 -0600

    Version 1. User has one guess.

--- src/guessgame/GuessGame.java
+++ src/guessgame/GuessGame.java
@@ -1,6 +1,22 @@
 package guessgame;
-public class GuessGame {
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessGame{ 
     public static void main(String[] args) {
-    }
+    Scanner userInput = new Scanner(System.in);
+    int guess;
+    Random rand = new Random();
+    int randomNumber = rand.nextInt(20) + 0;
+
+    System.out.print("I'm thinking of a number between 0-20. What is the number? ");
+    guess = userInput.nextInt();
     
+    if (guess == randomNumber) {
+        System.out.println("You got it!");
+    }
+    else {
+        System.out.println("Nop. The number is " + randomNumber + ".");
+    }
+    }
 }
